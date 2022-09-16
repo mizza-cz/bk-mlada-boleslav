4b93db8e9551256577517ddba3b02f738b7c3c78
mizza-cz <mizza.cz666@gmail.com>
2022-09-16 12:45:34 +0200

    add fix

---
+++ src/components/matches-section/matches-section.js
@@ -0,0 +1,12 @@
+$("#btn_games_collapse").click(function () {
+  $("#games_collapse").collapse("toggle");
+});
+$("#btn_games_o_pohar").click(function () {
+  $("#games_o_pohar").collapse("toggle");
+});
+$("#btn_games_collapsed").click(function () {
+  $("#games_collapsed").collapse("toggle");
+});
+$("#btn-games-calendar-collapse").click(function () {
+  $("#games-calendar-collapse").collapse("toggle");
+});
